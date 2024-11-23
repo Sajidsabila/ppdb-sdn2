898f833bc8ae58c74251e351fc3f24979624b481
Sajidsabila <sajid.sabila@gmail.com>
2024-11-23 21:07:40 +0700

    blade templating

---
+++ app/Livewire/Backend/Admin/Dashboard/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Backend\Admin\Dashboard;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.backend.admin.dashboard.index');
+    }
+}
---
+++ resources/views/livewire/backend/admin/dashboard/index.blade.php
@@ -0,0 +1,8 @@
+<div class="col-12">
+    <div class="card">
+        <div class="card-header">Judul</div>
+        <div class="card-body">
+            <p>haloo</p>
+        </div>
+    </div>
+</div>
